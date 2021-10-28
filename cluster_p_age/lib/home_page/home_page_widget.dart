10711cfdfb45e87787c1afe3b8120da0767fa4d9
Abigail Waryanka <abbywaryanka@Abigails-MacBook-Air.local>
2021-10-28 11:06:22 -0400

    add my code for the cluster overview page and the node overview page

---
+++ cluster_p_age/lib/home_page/home_page_widget.dart
@@ -0,0 +1,372 @@
+import '../flutter_flow/flutter_flow_theme.dart';
+import '../flutter_flow/flutter_flow_util.dart';
+import '../flutter_flow/flutter_flow_widgets.dart';
+import '../node1/node1_widget.dart';
+import '../node2/node2_widget.dart';
+import '../node3/node3_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class HomePageWidget extends StatefulWidget {
+  HomePageWidget({Key key}) : super(key: key);
+
+  @override
+  _HomePageWidgetState createState() => _HomePageWidgetState();
+}
+
+class _HomePageWidgetState extends State<HomePageWidget> {
+  bool _loadingButton1 = false;
+  bool _loadingButton2 = false;
+  bool _loadingButton3 = false;
+  final scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldKey,
+      backgroundColor: Color(0xFFF5F5F5),
+      body: SafeArea(
+        child: Align(
+          alignment: AlignmentDirectional(0, -0.1),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Align(
+                alignment: AlignmentDirectional(0.9, 0),
+                child: Container(
+                  width: 200,
+                  height: 30,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 0),
+                    child: Text(
+                      'Date and time',
+                      style: FlutterFlowTheme.bodyText1.override(
+                        fontFamily: 'Poppins',
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Align(
+                alignment: AlignmentDirectional(0.05, -0.6),
+                child: Container(
+                  width: 300,
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFEEEEEE),
+                  ),
+                  child: Align(
+                    alignment: AlignmentDirectional(0, 0),
+                    child: Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Expanded(
+                            child: Align(
+                              alignment: AlignmentDirectional(0, -0.25),
+                              child: Text(
+                                'Cluster Name',
+                                textAlign: TextAlign.center,
+                                style: FlutterFlowTheme.bodyText1.override(
+                                  fontFamily: 'Poppins',
+                                  fontSize: 30,
+                                ),
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            child: Text(
+                              'Version',
+                              textAlign: TextAlign.center,
+                              style: FlutterFlowTheme.bodyText1.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 25,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            'UUID',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 20,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 20, 0, 0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 50, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(5, 0, 0, 20),
+                            child: Text(
+                              'Status',
+                              style: FlutterFlowTheme.title3.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(17, 0, 0, 20),
+                            child: Text(
+                              'Name',
+                              style: FlutterFlowTheme.title3.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(45, 0, 0, 20),
+                            child: Text(
+                              'Serial ',
+                              style: FlutterFlowTheme.title3.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 17,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(35, 0, 0, 20),
+                            child: Text(
+                              'Up Time',
+                              style: FlutterFlowTheme.title3.override(
+                                fontFamily: 'Poppins',
+                                fontSize: 17,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                          child: Icon(
+                            Icons.settings_outlined,
+                            color: Color(0xFF006D18),
+                            size: 24,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                          child: FFButtonWidget(
+                            onPressed: () async {
+                              setState(() => _loadingButton1 = true);
+                              try {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Node1Widget(),
+                                  ),
+                                );
+                              } finally {
+                                setState(() => _loadingButton1 = false);
+                              }
+                            },
+                            text: 'NODE',
+                            options: FFButtonOptions(
+                              width: 100,
+                              height: 40,
+                              color: Color(0xF2061163),
+                              textStyle: FlutterFlowTheme.subtitle2.override(
+                                fontFamily: 'Poppins',
+                                color: Colors.white,
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                width: 1,
+                              ),
+                              borderRadius: 12,
+                            ),
+                            loading: _loadingButton1,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                          child: Text(
+                            'Node Serial',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 0, 0, 0),
+                          child: Text(
+                            'Uptime',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Icon(
+                            Icons.settings_outlined,
+                            color: Color(0xFF6D0002),
+                            size: 24,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: FFButtonWidget(
+                            onPressed: () async {
+                              setState(() => _loadingButton2 = true);
+                              try {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Node2Widget(),
+                                  ),
+                                );
+                              } finally {
+                                setState(() => _loadingButton2 = false);
+                              }
+                            },
+                            text: 'NODE 2',
+                            options: FFButtonOptions(
+                              width: 100,
+                              height: 40,
+                              color: Color(0xF2061163),
+                              textStyle: FlutterFlowTheme.subtitle2.override(
+                                fontFamily: 'Poppins',
+                                color: Colors.white,
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                width: 1,
+                              ),
+                              borderRadius: 12,
+                            ),
+                            loading: _loadingButton2,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Text(
+                            'Node Serial',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Text(
+                            'Uptime',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Icon(
+                            Icons.settings_outlined,
+                            color: Color(0xFF006D18),
+                            size: 24,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: FFButtonWidget(
+                            onPressed: () async {
+                              setState(() => _loadingButton3 = true);
+                              try {
+                                await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => Node3Widget(),
+                                  ),
+                                );
+                              } finally {
+                                setState(() => _loadingButton3 = false);
+                              }
+                            },
+                            text: 'NODE 3',
+                            options: FFButtonOptions(
+                              width: 100,
+                              height: 40,
+                              color: Color(0xF2061163),
+                              textStyle: FlutterFlowTheme.subtitle2.override(
+                                fontFamily: 'Poppins',
+                                color: Colors.white,
+                              ),
+                              borderSide: BorderSide(
+                                color: Colors.transparent,
+                                width: 1,
+                              ),
+                              borderRadius: 12,
+                            ),
+                            loading: _loadingButton3,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Text(
+                            'Node Serial',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsetsDirectional.fromSTEB(10, 30, 0, 0),
+                          child: Text(
+                            'Uptime',
+                            style: FlutterFlowTheme.bodyText1.override(
+                              fontFamily: 'Poppins',
+                              fontSize: 17,
+                            ),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
